7d2c25289b53633daa08566584e0a09268d99dc8
Dkell88 <DarrenKelly1188@gmail.com>
2022-05-30 15:11:11 -0700

    Complete exercise 3

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -2,7 +2,13 @@ require_relative '../setup'
 require_relative './exercise_1'
 require_relative './exercise_2'
 
+puts
+puts
+puts "----------------------------------------"
 puts "Exercise 3"
-puts "----------"
-
+puts "----------------------------------------"
 # Your code goes here ...
+
+@store3 = Store.find_by(id: 3)
+@store3.destroy
+puts "The number of stores in the Stores table is: #{Store.count}"
