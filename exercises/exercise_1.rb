fe358cc73582b080d3b2913b6d4910aa779854b4
Dkell88 <DarrenKelly1188@gmail.com>
2022-05-30 15:38:31 -0700

    Complete exercise 4

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -1,4 +1,5 @@
 require_relative '../setup'
+require 'pp'
 puts
 puts
 puts "----------------------------------------"
@@ -23,9 +24,9 @@ puts "----------------------------------------"
 #   table.timestamps null: false
 # end
 
-s1 = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
-s2 = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
-s3 = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+@s1 = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+@s2 = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+@s3 = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
 
 # s1 = Store.create
 # name = "Burnaby"
--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -3,7 +3,31 @@ require_relative './exercise_1'
 require_relative './exercise_2'
 require_relative './exercise_3'
 
+puts
+puts
+puts "----------------------------------------"
 puts "Exercise 4"
-puts "----------"
+puts "----------------------------------------"
 
 # Your code goes here ...
+
+# s1 = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+# s2 = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+# s3 = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+@s4 = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+@s5 = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+@s6 = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel: true)
+
+@mens_stores.each do |key|
+  puts("The mens store in #{key.name} has an annual revenue of #{key.annual_revenue}")
+end
+# puts("This is a list of the mens apparel stores: #{@mens_stores}" )
+# pp @mens_stores
+
+@womens_stores = Store.where(womens_apparel: true, annual_revenue: 1000000..)
+
+@mens_stores.each do |key|
+  puts("The womens store in #{key.name} has an annual revenue of #{key.annual_revenue}")
+end
